123ce20144d73c1e41041e126c4503b4029c7bb5
leongwenxuan <leongwenxuan0@gmail.com>
2019-10-23 02:44:26 +0800

    first test commit on wx branch

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Testing on Wenxuan Branch
+
 export default function App() {
   return (
     <View style={styles.container}>
